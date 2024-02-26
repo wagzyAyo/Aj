83a1d9a366aa8f49d95502e4259e34517a957a76
Ayo <talktojmcvibes@gmail.com>
2024-02-26 14:36:31 +0100

    animate service section

---
+++ javascript/animate.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('.content-1').waypoint(function () {
+        $('.content-1').addClass
+        ('animate__animated animate__fadeInLeftBig')
+    }, {offset: '85%'})
+
+    $('.content-2').waypoint(function () {
+        $('.content-2').addClass
+        ('animate__animated animate__fadeInRight')
+    }, {offset: '85%'})
+
+    $('.content-3').waypoint(function () {
+        $('.content-3').addClass
+        ('animate__animated animate__fadeInLeftBig')
+    }, {offset: '85%'})
+
+    $('.content-4').waypoint(function () {
+        $('.content-4').addClass
+        ('animate__animated animate__fadeInRight')
+    }, {offset: '85%'})
+
+    $('.content-5').waypoint(function () {
+        $('.content-5').addClass
+        ('animate__animated animate__fadeInLeftBig')
+    }, {offset: '85%'})
+
+    $('.content-6').waypoint(function () {
+        $('.content-6').addClass
+        ('animate__animated animate__fadeInRight')
+    }, {offset: '85%'})
+})
