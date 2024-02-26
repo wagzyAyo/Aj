c99442b0696e18bb36e1c722a36f9b3e0b46ff76
Ayo <talktojmcvibes@gmail.com>
2024-02-26 19:36:53 +0100

    send mail with php

---
+++ mail.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $from = "info@ajaccountinglens.com"; 
+    $to = "talktojmcvibes@gmail.com";
+    $subject = "New message from $name: $subject";
+    $body = "Name: $name\nEmail: $email\n\nmessage: $message";
+
+    $headers = "From: $from \r\n"; 
+
+    $headers .= "Reply-To: $email \r\n"; 
+
+    if (mail($to, $subject, $body, $headers)) { 
+        header("Location: index.html");
+        exit; 
+    } else {
+        echo "Error: Unable to send message";
+    }
+}
+?>
